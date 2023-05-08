4f6d0357faf07bb241b1fc9f43a606cc3be74ab2
Allison <91382279+gilgameshone@users.noreply.github.com>
2023-05-08 15:20:28 +0900

    trying to update from upstream

--- keyboards/gmmk/pro/rev1/ansi/keymaps/vnmm/keymap.c
+++ keyboards/gmmk/pro/rev1/ansi/keymaps/vnmm/keymap.c
@@ -56,7 +56,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 #if defined(ENCODER_MAP_ENABLE)
+<<<<<<< HEAD
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
+=======
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
+>>>>>>> trying to update from upstream
     [WIN_BASE] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
     [MAC_BASE] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
     [WIN_FN] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS) },
