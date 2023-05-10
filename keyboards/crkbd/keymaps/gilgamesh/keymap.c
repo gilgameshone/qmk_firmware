d8e8798df2497dcce97084ffb50aa395518b93f2
Allison <91382279+gilgameshone@users.noreply.github.com>
2023-05-10 09:54:23 +0900

    reversed del and bksp

--- keyboards/crkbd/keymaps/gilgamesh/keymap.c
+++ keyboards/crkbd/keymaps/gilgamesh/keymap.c
@@ -259,7 +259,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------|
       KC_TRNS, KC_TRNS, XXXXXXX,     SYM, JP_HASH,                       JP_YEN,    KC_1,    KC_2,    KC_3,    KC_0,
   //|--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------|
-                                 XXXXXXX,     LWR, XXXXXXX,     KC_DEL,     SYM, KC_BSPC
+                                 XXXXXXX,     LWR, XXXXXXX,    KC_BSPC,     SYM,  KC_DEL
                              //`--------------------------'  `--------------------------'
   ),
   [_RSE] = LAYOUT_split_3x5_3(
